c50a7c870abe4a5e9421c016ec5da6941ea91adc
gdltosea <105324256+gdltosea@users.noreply.github.com>
2022-12-10 01:53:45 -0800

    update Automate SSH
    Adding to automation_project

--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
 import os
+import paramiko
 class Server:
     """ Server class for representing and manipulating servers. """
 
@@ -10,3 +11,27 @@ class Server:
         # TODO - Use os module to ping the server
         return os.system('ping' + ' ' + self.server_ip)
 
+
+    def __int__(self, server_ip, key_file, username, upgrade_command):
+        self.server_ip = server_ip
+        self.ping()
+        self.server_ip = server_ip
+        self.username = username
+        self.upgrade_command = upgrade_command
+        self.key_file = key_file
+
+    def ping(self):
+        print("The Server IP is", self.server_ip)
+        result = os.system("ping -n 5 %s" % self.server_ip)
+        return result
+
+    def upgraded(self):
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddpolicy())
+        k = paramiko.RSAkey.from_private_key_file(self.key_file)
+        ssh_client.connect(hostname=self.server_ip, username=self.username, pkey=k)
+
+        stdin, stdout, stderr = ssh_client.exec_command(self.command)
+        result = stdout.readlines() + stderr.readlines()
+        ssh_client.close()
+        return result
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is the template code for the CNE335 Final Project
-# Justin Ellis
+# Juan Flores
 # CNE 335 Fall
 
 def print_program_info():
@@ -9,11 +9,24 @@ def print_program_info():
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
+    #TODO - Create a Server object
     from Server import Server
-    server_ip = '172.31.4.114'
+    server_ip = '104.193.114.103'
     y = Server(server_ip)
     # TODO - Call Ping method and print the results
     y.ping()
+
+
+    my_server_ip = '172.31.4.114'
+    my_rsa_key_file = r"C:\Users\JuanFlores\.ssh\id_rsa"
+    user_name = "ubuntu"
+    my_upgrade_command = 'sudo apt update && sudo apt upgrade -y'
+    my_server = Server(my_server_ip, my_rsa_key_file, user_name, my_upgrade_command)
+    print(my_server.ping())
+    print("Updating sever")
+    ssh_result = my_server.upgrade()
+    print(".join(ssh_result")
+
+
 
 
